0c5fe84703321fefe1af5f034ec640aa8ba66121
Philip Michael Raab <philip@cathedral.co.za>
2026-04-15 20:09:24 +0200

    ghq

---
+++ ohmyzsh/custom/061-cmd-ghq.zsh
@@ -0,0 +1,14 @@
+#########################################
+#		oh-my-zsh - custom: 005: aliases
+# version: 1                 2025 Aug 08
+#############################-###########
+
+## command
+# ghq
+#####################################################################
+if which ghq >/dev/null; then
+	function cdghq() {
+		local repo=$(ghq list | fzf)
+		[ -n "$repo" ] && cd "$(ghq root)/$repo"
+	}
+fi
