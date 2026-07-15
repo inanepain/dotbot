df402b3979ef1f105ecc6400c78f0658b5e405f8
Philip Michael Raab <philip@cathedral.co.za>
2026-07-15 21:33:01 +0200

    zsh.d

---
+++ home/zsh.d/kycx.zsh
@@ -0,0 +1,4 @@
+##################################################
+## zsh: config
+##################################################
+plugins+=($plugins eza zsh-interactive-cd zsh-navigation-tools zsh-autosuggestions zsh-syntax-highlighting)
