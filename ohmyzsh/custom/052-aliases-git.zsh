0cfcf453e05bfdafa825a3436c06690c22f47a2c
Philip Michael Raab <philip@cathedral.co.za>
2026-07-24 18:35:32 +0200

    added custom git aliases

--- ohmyzsh/custom/052-aliases-git.zsh
+++ ohmyzsh/custom/052-aliases-git.zsh
@@ -1,5 +1,16 @@
-# alias git submodule
+##################################################
+########## alias git submodule
+##################################################
 
+##################################################
+########## create custom git aliases
+##################################################
 (( ! $+aliases[gSf] )) && alias gSf="git submodule foreach --recursive"
 (( ! $+aliases[gpa] )) && alias gpa="git push --all"
 (( ! $+aliases[gpt] )) && alias gpt="git push --tags"
+
+##################################################
+########## repurpose existing git aliases
+##################################################
+(( $+aliases[gd] )) && unalias gd
+alias gd="git difftool --tool=araxis"
